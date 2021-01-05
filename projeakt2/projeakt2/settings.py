506f67fa15b41d1b076b53ad778d17560a82391f
msuban14 <msuban14@raf.rs>
2021-01-05 03:55:09 +0100

    DB connected

--- projeakt2/projeakt2/settings.py
+++ projeakt2/projeakt2/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'projeakt2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'projekat_2',
+        'USER': 'root',
+        'PASSWORD': 'Rootpassword1!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
